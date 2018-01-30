ebfc75a37a9e38f0899ef9437bf599933ee19412
ThoSap <sapelza.thomas@gmail.com>
2018-01-30 09:15:15 +0100

    jQueryMobile: Finished assignment 6

---
+++ AbgabenjQueryMobile/Übung_jQueryMobile/static/js/myScript.js
@@ -0,0 +1,23 @@
+$(function() {
+	alert("So");
+	//jQuery Funktion wird aufgerufen
+	$(document).on('pageshow','#seite1',function(event) {
+		// Die Funktion wird erst gestartet, wajax_aufrufenn die Seite (page) mit der ID [ID_EINTRAGEN]
+		// aufgerufen wird. In diese Seite soll später der Ajax-Content geladen werden.
+		
+		// alert("Diese Funktion startet nach dem Aufruf");
+		
+		$("#ajax_aufruf").load("bilder.html", function() {
+			// der div-Container mit der id [ID_EINTRAGEN], 
+			// welcher als Markierung im Dokument dient wird ausgewählt
+			
+			// es wird dann eine HTML-Datei [QUELLDATEI.html] zu der Markierung
+			// nachgeladen
+			
+			$('#seite1').trigger('create');
+			
+			// nach dem erfolgreichen Ajax-Request muss jQuery mobile die modifizierte Unterseite 
+			// mit der id [ID_EINTRAGEN] neu stylen
+		});	
+	})  
+});
